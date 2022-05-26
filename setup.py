212c5807e5a63853640a34dd49eed49c2e37a223
dougiesquire <dougiesquire@gmail.com>
2022-05-26 11:25:49 +1000

    setting up env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Codes for exploring Pearson correlation inference',
+    author='Dougie Squire',
+    license='MIT',
+)
---
+++ src/ar_model.py
@@ -0,0 +1,6 @@
+import statsmodels.api as sm
+from statsmodels.tsa.ar_model import ar_select_order
+from statsmodels.tsa.arima_process import ArmaProcess
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, plot_predict
+
+
